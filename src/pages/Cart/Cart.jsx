8a0d5c7c360c270d46c471b76e5099fc978bfc79
Andrii Pr <a.v.prykhodko25@gmail.com>
2026-03-13 17:28:48 +0100

    feat: improve cart page

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -23,8 +23,6 @@ const Cart = () => {
     const handleOrder = () => {
         if (items.length > 0) {
             navigate('/order/new')
-        } else {
-            console.log("Cart is empty!")
         }
     }
 
@@ -34,16 +32,37 @@ const Cart = () => {
 
             <h1 className={'cart__title'}>Your cart, {name? name : 'guest'}</h1>
 
-            <div>
-                {items.map((item) => {
-                    return <CartProduct key={item.id} product={item}/>
-                })}
-            </div>
+            {items.length === 0 ? (
+                <div className={'cart__empty'}>
+                    <p className={'cart__empty-message'}>
+                        Your cart is empty. Go to the menu to add pizzas.
+                    </p>
+                    <ProductButton
+                        onClick={() => navigate('/menu')}
+                        text={'GO TO MENU'}
+                    />
+                </div>
+            ) : (
+                <div>
+                    {items.map((item) => {
+                        return <CartProduct key={item.id} product={item}/>
+                    })}
+                </div>
+            )}
 
-            <div className={'cart__buttons'}>
-                <ProductButton onClick={handleOrder} text={'ORDER PIZZAS'}></ProductButton>
-                <ProductButton onClick={handleClearCart} text={'CLEAR CART'} isPrimary={false}></ProductButton>
-            </div>
+            {items.length > 0 && (
+                <div className={'cart__buttons'}>
+                    <ProductButton
+                        onClick={handleOrder}
+                        text={'ORDER PIZZAS'}
+                    ></ProductButton>
+                    <ProductButton
+                        onClick={handleClearCart}
+                        text={'CLEAR CART'}
+                        isPrimary={false}
+                    ></ProductButton>
+                </div>
+            )}
         </div>
     );
 };
